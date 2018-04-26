e653a3e417d1a78d89611a513fb855d26e54032e
Juan Ignacio Murcia <juanignaciomurcia@gmail.com>
2018-04-26 19:29:49 -0300

    Animacion de Cargando Terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { LoginPage } from '../pages/login/login';
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthService } from '../providers/auth-service/auth-service';
+import { SplashAnimadoPage } from '../pages/splash-animado/splash-animado';
 
 //import firebase from 'firebase';
 
@@ -15,12 +16,14 @@ import { AuthService } from '../providers/auth-service/auth-service';
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private auth: AuthService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private auth: AuthService,modalCtrl: ModalController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      let splash = modalCtrl.create(SplashAnimadoPage);
+            splash.present();
       
     });
 
---
+++ src/pages/splash-animado/splash-animado.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the SplashAnimadoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-splash-animado',
+  templateUrl: 'splash-animado.html',
+})
+export class SplashAnimadoPage {
+  progress:number;
+
+  constructor(public navCtrl: NavController,public viewCtrl: ViewController, public navParams: NavParams) {
+    this.progress = 0;  
+    let interval = window.setInterval(() => {
+      this.progress = this.progress+1;
+      if (this.progress >= 100) {
+          window.clearInterval(interval);
+      }
+    }, 50);
+  }
+  ionViewDidEnter() {
+ 
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+    }, 5000);
+ 
+  }
+
+
+}
